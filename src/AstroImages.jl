f979233e6131bb32ef9a3cb6ac362e03c33e3742
Mosè Giordano <mose@gnu.org>
2018-03-12 16:38:47 +0100

    Add AstroImage struct

--- src/AstroImages.jl
+++ src/AstroImages.jl
@@ -2,7 +2,7 @@ __precompile__()
 
 module AstroImages
 
-using FITSIO, FileIO
+using FITSIO, FileIO, Images
 
 export load
 
@@ -11,6 +11,10 @@ FileIO.load(f::File{format"FITS"}, ext::Int) = read(FITS(f.filename)[ext])
 function FileIO.load(f::File{format"FITS"}, ext::NTuple{N,Int}) where {N}
     fits = FITS(f.filename)
     return ntuple(i -> read(fits[ext[i]]), N)
+end
+
+struct AstroImage{T<:Color}
+    data::Matrix{T}
 end
 
 end # module
