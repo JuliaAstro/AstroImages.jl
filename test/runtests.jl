45e9884a5cef78f501a9a570263b2d20ac67e416
Mosè Giordano <mose@gnu.org>
2018-03-12 23:16:28 +0100

    Add tests for images and loading FITS files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,4 @@
-using AstroImages, FixedPointNumbers
+using AstroImages, FITSIO, Images
 using Base.Test
 
 import AstroImages: _float
@@ -31,4 +31,20 @@ import AstroImages: _float
             @test _float(typemax(T)) === FT(1 - big(2.0) ^ (1 - N))
         end
     end
+end
+
+@testset "FITS and images" begin
+    fname = tempname() * ".fits"
+    for T in [UInt8, Int8, UInt16, Int16, UInt32, Int32, Int64,
+              Float32, Float64]
+        data = reshape(T[1:100;], 5, 20)
+        FITS(fname, "w") do f
+            write(f, data)
+        end
+        @test load(fname) == data
+        @test load(fname, (1, 1)) == (data, data)
+        img = AstroImage(fname)
+        @test img.data == Gray.(_float.(data))
+    end
+    rm(fname, force=true)
 end
