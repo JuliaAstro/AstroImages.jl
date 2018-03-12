845ecc407649977eee871dda821e939eac1e16b2
Mosè Giordano <mose@gnu.org>
2018-03-12 11:14:14 +0100

    Add basic functions to read FITS extensions

--- src/AstroImages.jl
+++ src/AstroImages.jl
@@ -1,5 +1,16 @@
+__precompile__()
+
 module AstroImages
 
-# package code goes here
+using FITSIO, FileIO
+
+export load
+
+FileIO.load(f::File{format"FITS"}, ext::Int) = read(FITS(f.filename)[ext])
+
+function FileIO.load(f::File{format"FITS"}, ext::NTuple{N,Int}) where {N}
+    fits = FITS(f.filename)
+    return ntuple(i -> read(fits[ext[i]]), N)
+end
 
 end # module
