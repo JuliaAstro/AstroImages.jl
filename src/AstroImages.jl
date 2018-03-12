e9a575cdc074da0314ef57e38189c2520880c921
Mosè Giordano <mose@gnu.org>
2018-03-12 18:39:05 +0100

    Add utilities to convert numbers to float or fixed-point numbers

--- src/AstroImages.jl
+++ src/AstroImages.jl
@@ -13,6 +13,24 @@ function FileIO.load(f::File{format"FITS"}, ext::NTuple{N,Int}) where {N}
     return ntuple(i -> read(fits[ext[i]]), N)
 end
 
+# Images.jl expects data to be either a float or a fixed-point number.  Here we define some
+# utilities to convert all data types supported by FITS format to float or fixed-point:
+#
+#   * Float numbers are left as they are
+#   * Signed integers are mapped to [-1, 1) with Fixed type
+#   * Unsigned integers are mapped to [0, 1] with Normed type
+_float(x::AbstractFloat) = x
+for n in (8, 16, 32, 64)
+    SIT = Symbol("Int", n) # signed integer type
+    FPT = Expr(:curly, :Fixed, SIT, n-1) # fixed-point type for signed float
+    UIT = Symbol("UInt", n) # unsigned integer type
+    NIT = Symbol("N0f", n) # fixed-point type for unsigned float
+    @eval begin
+        _float(x::$SIT) = reinterpret($FPT, x)
+        _float(x::$UIT) = reinterpret($NIT, x)
+    end
+end
+
 struct AstroImage{T<:Color}
     data::Matrix{T}
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,34 @@
-using AstroImages
+using AstroImages, FixedPointNumbers
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+import AstroImages: _float
+
+@testset "Conversion to float and fixed-point" begin
+    @testset "Float" begin
+        for T in (Float16, Float32, Float64)
+            @test _float(T(-9.8)) === T(-9.8)
+            @test _float(T(12.3)) === T(12.3)
+        end
+    end
+    @testset "Unsigned integers" begin
+        for (T, NT) in ((UInt8,  N0f8),
+                        (UInt16, N0f16),
+                        (UInt32, N0f32),
+                        (UInt64, N0f64))
+            @test _float(typemin(T)) === NT(0)
+            @test _float(T(85)) === reinterpret(NT, T(85))
+            @test _float(typemax(T)) === NT(1)
+        end
+    end
+    @testset "Signed integers" begin
+        for T in (Int8, Int16, Int32, Int64)
+            N = sizeof(T) * 8
+            FT = Fixed{T, N-1}
+            @test _float(typemin(T)) === FT(-1)
+            @test _float(T(-85))     === reinterpret(FT, T(-85))
+            @test _float(T(0))       === reinterpret(FT, T(0))
+            @test _float(T(115))     === reinterpret(FT, T(115))
+            @test _float(typemax(T)) === FT(1 - big(2.0) ^ (1 - N))
+        end
+    end
+end
