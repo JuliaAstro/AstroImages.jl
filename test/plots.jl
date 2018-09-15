6c396bee508de875e20b769041074a32c6cb95b9
Mosè Giordano <mose@gnu.org>
2018-09-15 15:05:24 +0200

    Require Julia 0.7

--- test/plots.jl
+++ test/plots.jl
@@ -4,8 +4,8 @@ using RecipesBase
     data = randn(10, 10)
     img = AstroImage(data)
     rec = RecipesBase.apply_recipe(Dict{Symbol, Any}(), img)
-    @test rec[1].d == Dict{Symbol, Any}(:seriestype   => :heatmap,
-                                        :aspect_ratio => :equal,
-                                        :color        => :grays)
+    @test getfield(rec[1], 1) == Dict{Symbol, Any}(:seriestype   => :heatmap,
+                                                   :aspect_ratio => :equal,
+                                                   :color        => :grays)
     @test rec[1].args == (img.data,)
 end
