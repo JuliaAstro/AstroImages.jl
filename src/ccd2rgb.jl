6f07e49e0e186164320f86c2ba65d6ea4e8e7ed9
aquatiko <rohit970819@gmail.com>
2019-08-08 10:07:35 +0530

    new dispatch

--- src/ccd2rgb.jl
+++ src/ccd2rgb.jl
@@ -1,5 +1,7 @@
 """
     ccd2rgb(red::ImageHDU, green::ImageHDU, blue::ImageHDU; stretch = identity, shape_out = size(red))
+    ccd2rgb(red::Tuple{AbstractMatrix, WCSTransform}, green::Tuple{AbstractMatrix, WCSTransform},
+                     blue::Tuple{AbstractMatrix, WCSTransform}; stretch = identity, shape_out = size(red[1]))
 
 Converts 3 grayscale ImageHDU into RGB by reprojecting them.
 
@@ -21,10 +23,11 @@ julia> ccd2rgb(r, b, g, shape_out = (1000,1000), stretch = sqrt)
 julia> ccd2rgb(r, b, g, shape_out = (1000,1000), stretch = asinh)
 ```
 """
-function ccd2rgb(red::ImageHDU, green::ImageHDU, blue::ImageHDU; stretch = identity, shape_out = size(red))
-    red_rp = reproject(red, red, shape_out = shape_out)[1]
-    green_rp = reproject(green, red, shape_out = shape_out)[1]
-    blue_rp = reproject(blue, red, shape_out = shape_out)[1]
+function ccd2rgb(red::Tuple{AbstractMatrix, WCSTransform}, green::Tuple{AbstractMatrix, WCSTransform},
+                     blue::Tuple{AbstractMatrix, WCSTransform}; stretch = identity, shape_out = size(red[1]))
+    red_rp = reproject(red, red[2], shape_out = shape_out)[1]
+    green_rp = reproject(green, red[2], shape_out = shape_out)[1]
+    blue_rp = reproject(blue, red[2], shape_out = shape_out)[1]
     
     I = (red_rp .+ green_rp .+ blue_rp) ./ 3
     I .= (x -> stretch(x)/x).(I)
@@ -38,3 +41,8 @@ function ccd2rgb(red::ImageHDU, green::ImageHDU, blue::ImageHDU; stretch = ident
     m3 = maximum(x->isnan(x) ? -Inf : x, blue_rp)
     return colorview(RGB, red_rp./m1 , green_rp./m2, blue_rp./m3)
 end
+
+ccd2rgb(red::ImageHDU, green::ImageHDU, blue::ImageHDU; stretch = identity, shape_out = size(red)) =
+    ccd2rgb((read(red), WCS.from_header(read_header(red, String))[1]), (read(green), WCS.from_header(read_header(green, String))[1]),
+            (read(blue), WCS.from_header(read_header(blue, String))[1]), stretch = stretch, shape_out = shape_out)
+
