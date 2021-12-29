73d427294d7982c950f6249a52c8972eb861d2d6
William Thompson <wthompson@uvic.ca>
2021-12-29 10:49:36 -0800

    ds9 stretches and imshow edge cases

--- src/showmime.jl
+++ src/showmime.jl
@@ -1,25 +1,25 @@
-_brightness_contrast(color, matrix::AbstractMatrix{T}, brightness, contrast) where {T} =
-    @. color(matrix / 255 * T(contrast) + T(brightness) / 255)
+# _brightness_contrast(color, matrix::AbstractMatrix{T}, brightness, contrast) where {T} =
+#     @. color(matrix / 255 * T(contrast) + T(brightness) / 255)
 
-"""
-    brightness_contrast(image::AstroImage; brightness_range = 0:255, contrast_range = 1:1000, header_number = 1)
+# """
+#     brightness_contrast(image::AstroImage; brightness_range = 0:255, contrast_range = 1:1000, header_number = 1)
 
-Visualize the fits image by changing the brightness and contrast of image.
+# Visualize the fits image by changing the brightness and contrast of image.
 
-Users can also provide their own range as keyword arguments.
-"""
-function brightness_contrast(img::AstroImage{T,N}; brightness_range = 0:255,
-                             contrast_range = 1:1000, header_number = 1) where {T,N}
-    @manipulate for brightness  in brightness_range, contrast in contrast_range
-        _brightness_contrast(C, img.data[header_number], brightness, contrast)
-    end
-end
-
-# This is used in Jupyter notebooks
-Base.show(io::IO, mime::MIME"text/html", img::AstroImage; kwargs...) =
-    show(io, mime, brightness_contrast(img), kwargs...)
+# Users can also provide their own range as keyword arguments.
+# """
+# function brightness_contrast(img::AstroImage{T,N}; brightness_range = 0:255,
+#                              contrast_range = 1:1000, header_number = 1) where {T,N}
+#     @manipulate for brightness  in brightness_range, contrast in contrast_range
+#         _brightness_contrast(C, img.data[header_number], brightness, contrast)
+#     end
+# end
 
 # This is used in Jupyter notebooks
+# Base.show(io::IO, mime::MIME"text/html", img::AstroImage; kwargs...) =
+#     show(io, mime, brightness_contrast(img), kwargs...)
+
+# This is used in VSCode and others
 Base.show(io::IO, mime::MIME"image/png", img::AstroImage; kwargs...) =
     show(io, mime, imshow(img), kwargs...)
 
@@ -39,6 +39,110 @@ end
 function set_clims!(clims)
     _default_clims[] = clims
 end
+
+skipmissingnan(itr) = Iterators.filter(el->!ismissing(el) && isfinite(el), itr)
+
+# These reproduce the behaviour of DS9 according to http://ds9.si.edu/doc/ref/how.html
+logstretch(x,a=1000) = log(a*x+1)/log(a)
+powstretch(x,a=1000) = (a^x - 1)/a
+sqrtstretch = sqrt
+squarestretch(x) = x^2
+asinhstretch(x) = asinh(10x)/3
+sinhstretch(x) = sinh(3x)/10
+# The additional stretches reproduce behaviour from astropy
+powerdiststretch(x, a=1000) = (a^x - 1) / (a - 1)
+export logstretch, powstretch, sqrtstretch, squarestretch, asinhstretch, sinhstretch, powerdiststretch
+
+function imshow(
+    img::AbstractMatrix{T};
+    clims=_default_clims[],
+    stretch=identity,
+    cmap=_default_cmap[],
+) where {T}
+    isempt = isempty(img)
+    if isempt
+        return
+    end
+    # Users will occaisionally pass in data that is 0D, filled with NaN, or filled with missing.
+    # We still need to do something reasonable in those caes.
+    nonempty = any(x-> !ismissing(x) && isfinite(x), img)
+    if !nonempty
+        return
+    end
+    # Users can pass clims as an array or tuple containing the minimum and maximum values
+    if typeof(clims) <: AbstractArray || typeof(clims) <: Tuple
+        if length(clims) != 2
+            error("clims must have exactly two values if provided.")
+        end
+        imgmin = convert(T, first(clims))
+        imgmax = convert(T, last(clims))
+    # Or as a callable that computes them given an iterator
+    else
+        if nonempty
+            imgmin_0, imgmax_0 = clims(skipmissingnan(img))
+        else
+            # Fallback for empty images
+            imgmin_0, imgmax_0 = 0,1
+        end
+        imgmin = convert(T, imgmin_0)
+        imgmax = convert(T, imgmax_0)
+    end
+    normed = normedclampedview(img, (imgmin, imgmax))
+    return _imshow(normed,stretch,cmap)
+end
+function _imshow(normed::AbstractArray{T}, stretch, cmap) where T
+    if T <: Union{Missing,<:Number}
+        TT = typeof(first(skipmissing(normed)))
+    else
+        TT = T
+    end
+    # minstep(::Type{T}) where {T<:AbstractFloat} = eps(T)
+    # minstep(::Type{Bool}) = false
+    # minstep(T) = one(T)
+    # stretchmin = convert(TT, stretch(zero(TT)+minstep(TT)))
+    # stretchmax = convert(TT, stretch(one(T)))
+    stretchmin = 0
+    stretchmax = 1
+    # if T == Bool
+    #     Tout = N0f8
+    # else
+        Tout = T
+    # end
+
+    # No color map
+    if isnothing(cmap)
+        f = scaleminmax(stretchmin, stretchmax)
+        return mappedarray(normed) do pix
+            if ismissing(pix)
+                return Gray{TT}(0)
+            else
+                stretched = isfinite(pix) ? stretch(pix) : pix
+                return Gray{TT}(f(stretched))
+            end
+        end
+    # Monochromatic image using a colormap
+    else
+        cscheme = ColorSchemes.colorschemes[cmap]
+        return mappedarray(normed) do pix
+            stretched = !ismissing(pix) && isfinite(pix) ? stretch(pix) : pix
+            # We treat NaN/missing values as transparent
+            return if ismissing(stretched) || !isfinite(stretched)
+                RGBA{TT}(0,0,0,0)
+            # We treat Inf values as white / -Inf as black
+        elseif isinf(stretched)
+                if stretched > 0
+                    RGBA{TT}(1,1,1,1)
+                else
+                    RGBA{TT}(0,0,0,1)
+                end
+            else
+                RGBA{TT}(get(cscheme::ColorScheme, stretched, (stretchmin, stretchmax)))
+            end
+        end
+    end
+
+end
+export imshow
 
 """
     percent(99.5)
@@ -60,62 +164,38 @@ function percent(perc::Number)
 end
 export percent
 
-function imshow(
-    img::AbstractMatrix{T};
-    clims=_default_clims[],
-    cmap=_default_cmap[]
-) where {T}
-    # Users can pass clims as an array or tuple containing the minimum and maximum values
-    if typeof(clims) <: AbstractArray || typeof(clims) <: Tuple
-        if length(clims) != 2
-            error("clims must have exactly two values if provided.")
-        end
-        imgmin = convert(T, first(clims))
-        imgmax = convert(T, last(clims))
-    # Or as a callable that computes them given an iterator
-    else
-        imgmin_0, imgmax_0 = clims(Iterators.filter(pix->isfinite(pix) && !ismissing(pix), img))
-        imgmin = convert(T, imgmin_0)
-        imgmax = convert(T, imgmax_0)
-    end
-    return imshow(img,(imgmin,imgmax),cmap)
+# TODO: is this the correct function to extend?
+# Instead of using a datatype like N0f32 to interpret integers as fixed point values in [0,1],
+# we use a mappedarray to map the native data range (regardless of type) to [0,1]
+Images.normedview(img::AstroImage{<:FixedPoint}) = img
+function Images.normedview(img::AstroImage{T}) where T
+    imgmin, imgmax = extrema(skipmissingnan(img))
+    Δ = abs(imgmax - imgmin)
+    Tout = _Float(T)
+    normeddata = mappedarray(
+        pix -> (pix - imgmin)/Δ,
+        pix_norm -> convert(T, pix_norm*Δ + imgmin),
+        img
+    )
+    return shareheaders(img, normeddata)
 end
-function imshow(img::AbstractMatrix{T}, clims::Union{<:AbstractArray{<:T},Tuple{T,T}}, cmap) where {T}
+export normedview
 
-    if length(clims) != 2
-        error("clims must have exactly two values if provided.")
-    end
-    imgmin, imgmax = clims
 
-    # Pure grayscale display
-    if isnothing(cmap)
-        f = scaleminmax(_float(imgmin), _float(max(imgmax, imgmax + one(T))))
-        return mappedarray(Gray ∘ f, img)
-    # Monochromatic image using a colormap
-    else
-        cscheme = ColorSchemes.colorschemes[cmap]
-        # We create a MappedArray that converts from image data
-        # to RGBA values on the fly according to a colorscheme.
-        return mappedarray(img) do pix
-            # We treat Inf values as white / -Inf as black
-            return if isinf(pix)
-                if pix > 0
-                    RGBA{T}(1,1,1,1)
-                else
-                    RGBA{T}(0,0,0,1)
-                end
-            # We treat NaN/missing values as transparent
-            elseif !isfinite(pix) || ismissing(pix)
-                RGBA{T}(0,0,0,0)
-            else
-                RGBA{T}(get(cscheme::ColorScheme, pix, (imgmin, imgmax)))
-            end
-        end
-    end
-
+function normedclampedview(img::AbstractArray{T}, lims) where T
+    imgmin, imgmax = lims
+    Δ = abs(imgmax - imgmin)
+    normeddata = mappedarray(
+        pix -> clamp((pix - imgmin)/Δ, 0, 1),
+        pix_norm -> convert(T, pix_norm*Δ + imgmin),
+        img
+    )
+    return maybe_shareheaders(img, normeddata)
 end
-export imshow
-
+function normedclampedview(img::AbstractArray{Bool}, lims)
+    return img
+end
+export normedclampedview
 
 # Lazily reinterpret the AstroImage as a Matrix{Color}, upon request.
 # By itself, Images.colorview works fine on AstroImages. But 
