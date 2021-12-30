80f7484feb6de4ceb8871b3700de806599eea5b2
William Thompson <wthompson@uvic.ca>
2021-12-30 09:58:07 -0800

    Added docstrings

--- src/showmime.jl
+++ src/showmime.jl
@@ -29,22 +29,43 @@ using ColorSchemes
 using PlotUtils: zscale
 export zscale
 
-const _default_clims = Ref{Any}(extrema)
 const _default_cmap  = Ref{Union{Symbol,Nothing}}(nothing)
+const _default_clims = Ref{Any}(extrema)
 const _default_stretch  = Ref{Any}(identity)
 
+"""
+    set_cmap!(cmap::Symbol)
+    set_cmap!(cmap::Nothing)
+
+Alter the default color map used to display images when using
+`imview` or displaying an AstroImage.
+"""
 function set_cmap!(cmap)
     _default_cmap[] = cmap
 end
+"""
+    set_clims!(clims::Tuple)
+    set_clims!(clims::Function)
 
+Alter the default limits used to display images when using
+`imview` or displaying an AstroImage.
+"""
 function set_clims!(clims)
     _default_clims[] = clims
 end
+"""
+    set_stretch!(stretch::Function)
 
+Alter the default value stretch functio used to display images when using
+`imview` or displaying an AstroImage.
+"""
 function set_stretch!(stretch)
     _default_stretch[] = stretch
 end
 
+"""
+Helper to iterate over data skipping missing and non-finite values.
+""" 
 skipmissingnan(itr) = Iterators.filter(el->!ismissing(el) && isfinite(el), itr)
 
 # These reproduce the behaviour of DS9 according to http://ds9.si.edu/doc/ref/how.html
@@ -54,16 +75,70 @@ sqrtstretch = sqrt
 squarestretch(x) = x^2
 asinhstretch(x) = asinh(10x)/3
 sinhstretch(x) = sinh(3x)/10
-# The additional stretches reproduce behaviour from astropy
+# These additional stretches reproduce behaviour from astropy
 powerdiststretch(x, a=1000) = (a^x - 1) / (a - 1)
 export logstretch, powstretch, sqrtstretch, squarestretch, asinhstretch, sinhstretch, powerdiststretch
 
+"""
+    imview(img; clims=extrema, stretch=identity, cmap=nothing)
+
+Create a read only view of an array or AstroImage mapping its data values
+to Colors according to `clims`, `stretch`, and `cmap`.
+
+The data is first clamped to `clims`, which can either be a tuple of (min, max)
+values or a function accepting an iterator of pixel values that returns (min, max).
+By default, `clims=extrema` i.e. the minimum and maximum of `img`.
+Convenient functions to use for `clims` are:
+`extrema`, `zscale`, and `percent(p)`
+
+Next, the data is rescaled to [0,1] and remapped according to the function `stretch`.
+Stretch can be any monotonic fuction mapping values in the range [0,1] to some range [a,b].
+Note that `log(0)` is not defined so is not directly supported.
+For a list of convenient stretch functions, see:
+`logstretch`, `powstretch`, `squarestretch`, `asinhstretch`, `sinhstretch`, `powerdiststretch`
+
+Finally the data is mapped to RGB values according to `cmap`. If cmap is `nothing`,
+grayscale is used. ColorSchemes.jl defines hundreds of colormaps. A few nice ones for
+images innclude: `:viridis`, `:magma`, `:plasma`, `:thermal`, and `:turbo`.
+
+Crucially, this function returns a view over the underlying data. If `img` is updated
+then those changes will be reflected by this view with the exception of `clims` which
+is not recalculated.
+
+Note: if clims or stretch is a function, the pixel values passed in are first filtered
+to remove non-finite or missing values.
+
+### Defaults
+The default values of `clims`, `stretch`, and `cmap` are `extrema`, `identity`, and `nothing`
+respectively.
+You may alter these defaults using `AstroImages.set_clims!`,  `AstroImages.set_stretch!`, and
+`AstroImages.set_cmap!`.
+
+### Automatic Display
+Arrays wrapped by `AstroImage()` get displayed as images automatically by calling 
+`imview` on them with the default settings when using displays that support showing PNG images.
+
+### Missing data
+Pixels that are `NaN` or `missing` will be displayed as transparent when `cmap` is set
+or black if.
++/- Inf will be displayed as black or white respectively.
+
+### Exporting Images
+The view returned by `imview` can be saved using general `FileIO.save` methods.
+Example:
+```julia
+v = imview(data, cmap=:magma, stretch=asinhstretch, clims=percent(95))
+save("output.png", v)
+```
+"""
 function imview(
     img::AbstractMatrix{T};
     clims=_default_clims[],
     stretch=_default_stretch[],
     cmap=_default_cmap[],
 ) where {T}
+
+    # TODO: catch this in `show` instead of here.
     isempt = isempty(img)
     if isempt
         return
@@ -79,21 +154,14 @@ function imview(
         if length(clims) != 2
             error("clims must have exactly two values if provided.")
         end
-        imgmin = convert(T, first(clims))
-        imgmax = convert(T, last(clims))
+        imgmin = first(clims)
+        imgmax = last(clims)
     # Or as a callable that computes them given an iterator
     else
-        if nonempty
-            imgmin_0, imgmax_0 = clims(skipmissingnan(img))
-        else
-            # Fallback for empty images
-            imgmin_0, imgmax_0 = 0,1
-        end
-        imgmin = convert(T, imgmin_0)
-        imgmax = convert(T, imgmax_0)
+        imgmin, imgmax = clims(skipmissingnan(img))
     end
-    normed = normedclampedview(img, (imgmin, imgmax))
-    return _imview(img, normed,stretch,cmap)
+    normed = clampednormedview(img, (imgmin, imgmax))
+    return _imview(img, normed, stretch, cmap)
 end
 function _imview(img, normed::AbstractArray{T}, stretch, cmap) where T
     if T <: Union{Missing,<:Number}
@@ -101,20 +169,14 @@ function _imview(img, normed::AbstractArray{T}, stretch, cmap) where T
     else
         TT = T
     end
-    # minstep(::Type{T}) where {T<:AbstractFloat} = eps(T)
-    # minstep(::Type{Bool}) = false
-    # minstep(T) = one(T)
-    # stretchmin = convert(TT, stretch(zero(TT)+minstep(TT)))
-    # stretchmax = convert(TT, stretch(one(T)))
-    stretchmin = 0
-    stretchmax = 1
-    # if T == Bool
-    #     Tout = N0f8
-    # else
-        Tout = T
-    # end
+    if TT == Bool
+        TT = N0f8
+    end
 
-    # No color map
+    stretchmin = stretch(zero(TT))
+    stretchmax = stretch(one(TT))
+
+    # No color map: use Gray
     if isnothing(cmap)
         f = scaleminmax(stretchmin, stretchmax)
         mapper = mappedarray(normed) do pix
@@ -125,16 +187,22 @@ function _imview(img, normed::AbstractArray{T}, stretch, cmap) where T
                 return Gray{TT}(f(stretched))
             end
         end
-    # Monochromatic image using a colormap
+    # Monochromatic image using a colormap via ColorSchemes
     else
         cscheme = ColorSchemes.colorschemes[cmap]
-        mapper = mappedarray(normed) do pix
-            stretched = !ismissing(pix) && isfinite(pix) ? stretch(pix) : pix
+        mapper = mappedarray(img, normed) do pixr, pixn
+            if ismissing(pixr) || !isfinite(pixr) || ismissing(pixn) || !isfinite(pixn)
+                # We check pixr in addition to pixn because we want to preserve if the pixels
+                # are +-Inf
+                stretched = pixr
+            else
+                stretched = stretch(pixn)
+            end
             # We treat NaN/missing values as transparent
-            return if ismissing(stretched) || !isfinite(stretched)
+            return if ismissing(stretched) || isnan(stretched)
                 RGBA{TT}(0,0,0,0)
             # We treat Inf values as white / -Inf as black
-        elseif isinf(stretched)
+            elseif isinf(stretched)
                 if stretched > 0
                     RGBA{TT}(1,1,1,1)
                 else
@@ -179,7 +247,6 @@ Images.normedview(img::AstroImage{<:FixedPoint}) = img
 function Images.normedview(img::AstroImage{T}) where T
     imgmin, imgmax = extrema(skipmissingnan(img))
     Δ = abs(imgmax - imgmin)
-    Tout = _Float(T)
     normeddata = mappedarray(
         pix -> (pix - imgmin)/Δ,
         pix_norm -> convert(T, pix_norm*Δ + imgmin),
@@ -189,21 +256,44 @@ function Images.normedview(img::AstroImage{T}) where T
 end
 export normedview
 
+"""
+    clampednormedview(arr, (min, max))
 
-function normedclampedview(img::AbstractArray{T}, lims) where T
+Given an AbstractArray and limits `min,max` return a view of the array
+where data between [min, max] are scaled to [0, 1] and datat outside that
+range are clamped to [0, 1].
+
+See also: normedview
+"""
+function clampednormedview(img::AbstractArray{T}, lims) where T
     imgmin, imgmax = lims
     Δ = abs(imgmax - imgmin)
     normeddata = mappedarray(
-        pix -> clamp((pix - imgmin)/Δ, 0, 1),
+        pix -> clamp((pix - imgmin)/Δ, zero(T), one(T)),
         pix_norm -> convert(T, pix_norm*Δ + imgmin),
         img
     )
     return maybe_shareheaders(img, normeddata)
 end
-function normedclampedview(img::AbstractArray{Bool}, lims)
+function clampednormedview(img::AbstractArray{T}, lims) where T <: Normed
+    # If the data is in a Normed type and the limits are [0,1] then
+    # it already lies in that range.
+    if lims[1] == 0 && lims[2] == 1
+        return img
+    end
+    imgmin, imgmax = lims
+    Δ = abs(imgmax - imgmin)
+    normeddata = mappedarray(
+        pix -> clamp((pix - imgmin)/Δ, zero(T), one(T)),
+        pix_norm -> pix_norm*Δ + imgmin,
+        img
+    )
+    return maybe_shareheaders(img, normeddata)
+end
+function clampednormedview(img::AbstractArray{Bool}, lims)
     return img
 end
-export normedclampedview
+export clampednormedview
 
 # Lazily reinterpret the AstroImage as a Matrix{Color}, upon request.
 # By itself, Images.colorview works fine on AstroImages. But 
