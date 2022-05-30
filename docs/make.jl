077c1dfb9855443d8ad660cd266cd7fc9037ecc7
William Thompson <will.thompson@outlook.com>
2022-05-30 12:51:07 -0700

    Use DemoCards.jl for examples

--- docs/make.jl
+++ docs/make.jl
@@ -1,12 +1,30 @@
-using Documenter, AstroImages
+using Documenter, DemoCards, AstroImages
+
+# 1. generate demo files
+demopage, postprocess_cb, demo_assets = makedemos("examples") # this is the relative path to docs/
+
+# if there are generated css assets, pass it to Documenter.HTML
+assets = []
+isnothing(demo_assets) || (push!(assets, demo_assets))
+
+# 2. normal Documenter usage
+format = Documenter.HTML(assets = assets)
+makedocs(format = format,
+         pages = [
+            "Home" => "index.md",
+            demopage,
+         ],
+         sitename = "Awesome demos")
+
+
 makedocs(
     sitename="AstroImages.jl",
     pages = [
         "Home" => "index.md",
-        "Tour" => "tour.md",
-        "Tutorials" => [
+        "Manual" => [
             "Getting Started" => "getting-started.md"
         ],
+        demopage,
         "API" => "api.md",
     ],
     format = Documenter.HTML(
@@ -15,6 +33,8 @@ makedocs(
     workdir=".."
 )
 
+# 3. postprocess after makedocs
+postprocess_cb()
 
 # deploydocs(
 #     repo = "github.com/sefffal/AstroImages.jl.git",
